48a945f9cdb04362d2028471f6f7540977e5e15c
LeanKhan <eslean20@gmail.com>
2018-09-15 16:38:09 +0100

    Populate FixturesView with data

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,6 +9,31 @@ export class DataService {
 leagueTableUri = 'https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4328&s=1819';
 nextFixturesUri = 'https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328';
 
+teams:any = {
+  133604 : "ARS",
+  133610 : "CHE",
+  133613 : "MCI",
+  133612 : "MUN",
+  133602 : "LIV",
+  133624 : "WAT",
+  133626 : "LEI",
+  133616 : "TOT",
+  133636 : "WHU",
+  134777 : "NEW",
+  134778 : "SOU",
+  133632 : "CRY",
+  134301 : "BOU",
+  133615 : "EVE",
+  133600 : "FUL",
+  133637 : "CAR",
+  133932 : "HUD",
+  133623 : "BUR",
+  133619 : "BHA",
+  133599 : "WOL"
+
+
+}
+
   constructor(private _http:HttpClient) { }
 
   // Get the League table
--- src/app/home/fixtures/fixtures.component.ts
+++ src/app/home/fixtures/fixtures.component.ts
@@ -27,4 +27,40 @@ nextFixtures = [{alive:"I am alive"}];
     },err=>{console.error(err)},()=>{console.log("Done!")})
   }
 
+ showDate(eventDate:string){
+   let now = new Date();
+   let dateArray = eventDate.split("-");
+   let fixtureMonth = dateArray[1];
+   let fixtureDate = dateArray[2];
+   let fixtureYear = dateArray[0];
+
+   if(parseInt(fixtureYear) == now.getFullYear()){
+     if(parseInt(fixtureMonth) == (now.getMonth()+1)){
+       if(parseInt(fixtureDate) == now.getDate()){
+         return "Today at";
+       }else if(parseInt(fixtureDate) == (now.getDate()+1)){
+         return "Tomorrow at"
+       }else{return eventDate}
+     }
+   }
+
+ }
+
+//  Convert eventTime to Locale time
+
+toLocaleTime(eventTime){
+  let now = new Date();
+  let timezoneOffset = now.getTimezoneOffset();
+ 
+
+  let realTimeStr = eventTime.split('+')[0].split(':');
+    realTimeStr = realTimeStr.splice(0,2);
+
+  realTimeStr[0] = parseInt(realTimeStr[0]) + (timezoneOffset/-60);
+
+  return realTimeStr.join(":");
+
+
+}
+  
 }
